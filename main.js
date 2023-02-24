5a3f936164d2579125884c6da6fa7f98102919af
Geusen <89836078+Geusen@users.noreply.github.com>
2023-02-24 21:47:54 +0900

    [update]html更新

--- main.js
+++ main.js
@@ -5,16 +5,19 @@ window.onload = function() {
   }).then(function (json) {
     document.getElementById("date").innerText = '最終更新: ' + json[0].time;  // 最終更新日時を表示
 
+    // imgurのID取得
     const id = String(json[1].url)
     .replace("https://i.imgur.com/", "")
     .replace(".jpg", "")
     .replace(".png", "")
-    .replace(".jpeg", "");  // imgurのID取得
+    .replace(".jpeg", "");
 
+    // imgurの埋め込み
     document.getElementById("img").setAttribute("class", "imgur-embed-pub")
     document.getElementById("img").setAttribute("data-id", id);
     document.getElementById("img_js").setAttribute("src", "//s.imgur.com/min/embed.js");
 
+    // imgurのjsの読み込み
     const script = document.createElement('script');
     script.src = "//s.imgur.com/min/embed.js"; //ファイルパス
     document.head.appendChild(script); //<head>に生成
