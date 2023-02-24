8f53af108357d2269bf23e80477d2161d264741c
Geusen <89836078+Geusen@users.noreply.github.com>
2023-02-24 15:19:40 +0900

    [update]imgur対応

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 window.onload = function() {
-  fetch('https://script.google.com/macros/s/AKfycbzOrnK4MjzO4jDzDFtx9cCZPIaVM1LHhqsC_9AdzpjlTXHWon0rVJvYdSoDo-dL_sY2iA/exec')
+  fetch('https://script.google.com/macros/s/AKfycbxapSGVuHxmsMJtsdOiiWAT2xNnCr8YpCKa3pKTo_YMnHmdoYfZkBqhe2sEM-54JjonPA/exec')
   .then(function (response) {
-    return json = response.json()
+    return json = response.json() // responseをjsonに変換
   }).then(function (json) {
-    document.getElementById("date").innerText = '最終更新: ' + json[0].time;
+    document.getElementById("date").innerText = '最終更新: ' + json[0].time;  // 最終更新日時を表示
+
+    const id = String(json[1].url)
+    .replace("https://i.imgur.com/", "")
+    .replace(".jpg", "")
+    .replace(".png", "")
+    .replace(".jpeg", "");  // imgurのID取得
+
+    document.getElementById("img").setAttribute("class", "imgur-embed-pub")
+    document.getElementById("img").setAttribute("data-id", id);
+    document.getElementById("img_js").setAttribute("src", "//s.imgur.com/min/embed.js");
+
+    const script = document.createElement('script');
+    script.src = "//s.imgur.com/min/embed.js"; //ファイルパス
+    document.head.appendChild(script); //<head>に生成
   });
 }
